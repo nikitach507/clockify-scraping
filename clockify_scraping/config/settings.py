c613b0e25fdbd8f7c5b2d3bb8dd005a9889ec5dc
nikitach507 <chuprinnikita8@gmail.com>
2024-07-08 14:13:17 +0200

    Add config settings

---
+++ clockify_scraping/config/settings.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Settings:
+    CLOCKIFY_API_KEY = os.getenv('CLOCKIFY_API_KEY')
+    CLOCKIFY_BASE_URL = os.getenv('CLOCKIFY_BASE_URL')
+    CLOCKIFY_WORKSPACE_ID = os.getenv('CLOCKIFY_WORKSPACE_ID')
+    GOOGLE_SHEETS_CREDENTIALS_FILE = os.getenv('GOOGLE_SHEETS_CREDENTIALS_FILE')
+    GOOGLE_OAUTH_TOKEN_FILE = os.getenv('GOOGLE_OAUTH_TOKEN_FILE')
+    SPREADSHEET_ID = os.getenv('SPREADSHEET_ID')
+    SPREADSHEET_NAME = os.getenv('SPREADSHEET_NAME')
