e0c3440ef5abc15036bf5f16180d210fafcd2b44
nikitach507 <chuprinnikita8@gmail.com>
2024-07-17 10:40:33 +0200

    Change the search by "-" to an empty row

--- excelify/excelify/sheet_handler.py
+++ excelify/excelify/sheet_handler.py
@@ -4,26 +4,13 @@ from openpyxl import Workbook
 from openpyxl.utils import get_column_letter
 from openpyxl.worksheet.worksheet import Worksheet
 
-def verify_project_sheet(workbook: Workbook, sheet_name: str) -> None:
-    try:
-        workbook.get_sheet_by_name(sheet_name)
-        print(f"Sheet {sheet_name} already exists in the Excel file.")
-        exit(0)
-    except KeyError:
-        if len(sheet_name) > 31:
-            raise click.BadParameter('Sheet name is too long. Max 31 characters allowed.')
-        if re.search(r'[\/\\\?\*\:\[\]]', sheet_name):
-            raise click.BadParameter('Sheet name cannot contain / \\ ? * : [ ]')
-        
-        worksheet = workbook.create_sheet(title=sheet_name)
-        return worksheet
 
 def generate_total_rows(current_date: str, start_row: int, found_users: dict[str, str], num_rows: int = 96) -> list[list[str]]:
     total_formula_row = []
     for col_index, user in enumerate(found_users.values(), start=2): 
         column_letter = get_column_letter(col_index)
 
-        count_formula = f"COUNTIF({column_letter}{start_row}:{column_letter}{start_row + num_rows - 1}, \"<>-\")"
+        count_formula = f"COUNTIF({column_letter}{start_row}:{column_letter}{start_row + num_rows - 1}, \"<>\")"
         total_minutes = f"{count_formula} * 15"
         formatted_time_formula = f"=TEXT(INT({total_minutes} / 60), \"0\") & \":\" & TEXT(MOD({total_minutes}, 60), \"00\")"
         total_formula_row.append(formatted_time_formula)
